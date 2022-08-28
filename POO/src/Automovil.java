87ea4df464c657006ccb8fe4d37de98d8980812c
Antonio-eli <eliceoroan@gmail.com>
2022-08-28 13:27:40 -0500

    Add Agregando metodos

--- POO/src/Automovil.java
+++ POO/src/Automovil.java
@@ -4,4 +4,14 @@ public class Automovil {
     String color = "gris";
     double cilindrada;
 
+    public String detalle() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("auto.fabricante = " + this.fabricante + "\n");
+        sb.append("auto.modelo = " + this.modelo + "\n");
+        sb.append("auto.color = " + this.color + "\n");
+        sb.append("auto.cilindrada = " + this.cilindrada + "\n");
+
+        return sb.toString();
+    }
+
 }
