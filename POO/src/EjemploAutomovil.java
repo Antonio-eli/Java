5d4ca7b20f1cb3b957444530eaa95e1a64b73746
Antonio-eli <eliceoroan@gmail.com>
2022-08-28 16:11:11 -0500

    Add Sobrecarga de constructores

--- POO/src/EjemploAutomovil.java
+++ POO/src/EjemploAutomovil.java
@@ -4,19 +4,20 @@ public class EjemploAutomovil {
         subaru.setCilindrada(2.0);
         subaru.setColor("Blanco");
 
-        Automovil mazda = new Automovil("Mazda", "BT-50");
-        mazda.setCilindrada(3.0);
-        mazda.setColor("Rojo");
+        Automovil mazda = new Automovil("Mazda", "BT-50",  "Rojo", 3.0);
         System.out.println("mazda.getFabricante() = " + mazda.getFabricante());
 
-        Automovil nissan = new Automovil();
+        Automovil nissan = new Automovil("Nissan", "Navara", "Gris oscuro", 3.5, 50);
 
         System.out.println(subaru.detalle());
         System.out.println(mazda.detalle());
+        System.out.println(nissan.detalle());
         System.out.println(subaru.acelerar(3000));
         System.out.println(subaru.frenar());
         System.out.println(mazda.acelerarFrenar(4000));
-        System.out.println("Kilometros por litro " + subaru.calcularConusmo(300, 0.6f));
-        System.out.println("Kilometros por litro " + subaru.calcularConusmo(300, 60));
+        System.out.println("Kilometros por litro subaru " + subaru.calcularConusmo(300, 0.6f));
+        //Implementacion con sobre carga de metodos
+        System.out.println("Kilometros por litro de subaru " + subaru.calcularConusmo(300, 60));
+        System.out.println("Kilometros por litro de nissan " + nissan.calcularConusmo(300, 60));
     }
 }
