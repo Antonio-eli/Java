9d144dd23eb088385e3a55e30d817da81fa80ee5
Antonio-eli <eliceoroan@gmail.com>
2022-10-09 16:06:54 -0500

    Add clases abstractas Validador

---
+++ poo_clases_abstractas/src/com/coderroan/pooclasesabstractas/form/validador/LargoValidador.java
@@ -0,0 +1,43 @@
+package com.coderroan.pooclasesabstractas.form.validador;
+
+public class LargoValidador extends Validador{
+    protected String mensaje = "el campo debe tener minimo %d caracteres %d caracteres";
+    private int minimo = 0;
+    private int maximo = Integer.MAX_VALUE;
+
+    public LargoValidador() {
+    }
+
+    public LargoValidador(int minimo, int maximo) {
+        this.minimo = minimo;
+        this.maximo = maximo;
+    }
+
+    public void setMinimo(int minimo) {
+        this.minimo = minimo;
+    }
+
+    public void setMaximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+    @Override
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    @Override
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public boolean esValido(String valor) {
+        this.mensaje = String.format(this.mensaje, this.minimo, this.maximo);
+        if(valor == null){
+            return true;
+        }
+        int largo = valor.length();
+        return (largo >= minimo && largo <= maximo);
+    }
+}
