363f4c00a76c0bcb8b7a22230f13683d43d06c19
Antonio-eli <eliceoroan@gmail.com>
2022-10-16 11:46:35 -0500

    Add metodos genericos maximo

--- poo_interfaces_repositorio/src/com/coderroan/generics/EjemploGenericos.java
+++ poo_interfaces_repositorio/src/com/coderroan/generics/EjemploGenericos.java
@@ -35,6 +35,11 @@ public class EjemploGenericos {
         imprimirClientes(clientes);
         imprimirClientes(clienteLista);
         imprimirClientes(clientePremiumList);
+        System.out.println(ANSI_BLUE + "[ Métodos genéricos máximo ]" + ANSI_RESET);
+        System.out.println("Máximo de 1, 9 y 4 es: " + maximo(1,3,9));
+        System.out.println("Máximo de 3.9, 11.6 y 7.78 es: " + maximo(3.9, 11.6, 7.78));
+        System.out.println("Máximo de Zanahoria, Naranja y Manzana es: " + maximo("Zanahoria", "Naranja", "Manzana"));
+
 
     }
 
@@ -57,6 +62,14 @@ public class EjemploGenericos {
 
     public static void imprimirClientes(List<? extends Cliente> clientes){
         clientes.forEach(System.out::println);
+    }
+
+    public static <T extends Comparable<T>> T maximo(T a, T b, T c){
+        T max = a;
+        if (b.compareTo(max) > 0){ max = b; }
+        if (c.compareTo(max) > 0){ max = c; }
+
+        return max;
     }
 }
 
