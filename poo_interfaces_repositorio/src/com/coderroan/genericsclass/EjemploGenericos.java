5964fdaf8e6842dc26b7ad48745e0e4d9e0f6990
Antonio-eli <eliceoroan@gmail.com>
2022-10-16 14:21:23 -0500

    Change clase generica con parametro T

--- poo_interfaces_repositorio/src/com/coderroan/genericsclass/EjemploGenericos.java
+++ poo_interfaces_repositorio/src/com/coderroan/genericsclass/EjemploGenericos.java
@@ -3,37 +3,41 @@ package com.coderroan.genericsclass;
 public class EjemploGenericos {
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_RESET = "\u001B[0m";
+
+    public static <T> void imprimirCamion( Camion<T> camion ){
+        for (T t: camion){
+            if (t instanceof Animal){
+                System.out.println(((Animal)t).getNombre() + " tipo: " + ((Animal)t).getTipo());
+            }else if (t instanceof Automovil){
+                System.out.println(((Automovil)t).getMarca());
+            }else if (t instanceof Maquinaria){
+                System.out.println(((Maquinaria)t).getTipo());
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Camion transporteCaballos = new Camion(5);
+        Camion<Animal> transporteCaballos = new Camion<>(5);
         transporteCaballos.add(new Animal("Pegaso", "Caballo"));
         transporteCaballos.add(new Animal("Júpiter", "Caballo"));
         transporteCaballos.add(new Animal("Neptuno", "Caballo"));
         transporteCaballos.add(new Animal("Trueno", "Caballo"));
         transporteCaballos.add(new Animal("Príncipe", "Caballo"));
         System.out.println(ANSI_BLUE + "[ Transporte Caballos  ]" + ANSI_RESET);
-        for(Object o: transporteCaballos){
-            Animal a = (Animal) o;
-            System.out.println(a.getNombre() + " tipo: " + a.getTipo());
-        }
+        imprimirCamion(transporteCaballos);
 
-        Camion transporteMaquinaria = new Camion(3);
+        Camion<Maquinaria> transporteMaquinaria = new Camion<>(3);
         transporteMaquinaria.add(new Maquinaria("Excavadora"));
         transporteMaquinaria.add(new Maquinaria("Compactadora"));
         transporteMaquinaria.add(new Maquinaria("Retroexcavadora"));
         System.out.println(ANSI_BLUE + "[ Transporte Maquinaria  ]" + ANSI_RESET);
-        for (Object o: transporteMaquinaria){
-            Maquinaria m = (Maquinaria) o;
-            System.out.println(m.getTipo());
-        }
+        imprimirCamion(transporteMaquinaria);
 
-        Camion transporteAutomovil = new Camion(3);
+        Camion<Automovil> transporteAutomovil = new Camion<>(3);
         transporteAutomovil.add(new Automovil("Audi"));
         transporteAutomovil.add(new Automovil("BMW"));
         transporteAutomovil.add(new Automovil("Chevrolet"));
         System.out.println(ANSI_BLUE + "[ Transporte Automovil  ]" + ANSI_RESET);
-        for (Object o: transporteAutomovil){
-            Automovil a = (Automovil) o;
-            System.out.println(a.getMarca());
-        }
+        imprimirCamion(transporteAutomovil);
     }
 }
