a44427e3b842568cc9877575f61bb97c43ec78e3
Antonio-eli <eliceoroan@gmail.com>
2022-10-24 20:44:19 -0500

    Add ejemplo HashSet

---
+++ colecciones/src/com/coderroan/ejemplos/set/EjemploHashSetAgregar.java
@@ -0,0 +1,20 @@
+package com.coderroan.ejemplos.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EjemploHashSetAgregar {
+    public static void main(String[] args) {
+        Set<String> hs = new HashSet<>();
+        hs.add("uno");
+        hs.add("dos");
+        hs.add("tres");
+        hs.add("cuatro");
+        hs.add("cinco");
+        System.out.println(hs);
+        boolean b = hs.add("cinco");
+        System.out.println("\nPermite objetos duplicados? " + b);
+        System.out.println(hs);
+
+    }
+}
