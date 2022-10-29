671a6f9f0fd512b79c4889905263efba1128941b
Antonio-eli <eliceoroan@gmail.com>
2022-10-29 14:58:32 -0500

    Add jQuery Singularidad en TreeSet y HashSet

--- colecciones/src/com/coderroan/ejemplos/modelo/Alumno.java
+++ colecciones/src/com/coderroan/ejemplos/modelo/Alumno.java
@@ -2,6 +2,8 @@ package com.coderroan.ejemplos.modelo;
 
 import com.coderroan.ejemplos.set.Color;
 
+import java.util.Objects;
+
 public class Alumno implements Comparable<Alumno>{
     private String nombre;
     private Integer nota;
@@ -51,5 +53,18 @@ public class Alumno implements Comparable<Alumno>{
             return 0;
         }
         return this.nota.compareTo(a.nota);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return Objects.equals(nombre, alumno.nombre) && Objects.equals(nota, alumno.nota);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, nota);
     }
 }
--- colecciones/src/com/coderroan/ejemplos/set/EjemploHashSetUnicidad.java
+++ colecciones/src/com/coderroan/ejemplos/set/EjemploHashSetUnicidad.java
@@ -2,18 +2,21 @@ package com.coderroan.ejemplos.set;
 
 import com.coderroan.ejemplos.modelo.Alumno;
 
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
-public class EjemploTreeSetComparable {
+public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
-        Set<Alumno> sa = new TreeSet<>((a, b) -> a.getNombre().compareTo(b.getNombre()));
+        Set<Alumno> sa = new HashSet<>();
         sa.add(new Alumno("Eliceo", 9));
+        sa.add(new Alumno("Eliceo2", 9));
         sa.add(new Alumno("Raul", 7));
         sa.add(new Alumno("Manuel", 6));
         sa.add(new Alumno("Sandra", 4));
         sa.add(new Alumno("Agel", 5));
         sa.add(new Alumno("Brandon", 5));
+        sa.add(new Alumno("Luis", 5));
+        sa.add(new Alumno("Luis", 6));
         System.out.println("Alumnos = " + sa);
     }
 }
