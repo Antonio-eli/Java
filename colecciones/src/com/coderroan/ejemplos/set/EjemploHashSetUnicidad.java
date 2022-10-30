4172d040d20e81ed87686cf4ea4a911fb27177c1
Antonio-eli <eliceoroan@gmail.com>
2022-10-30 12:37:27 -0600

    Add loop o iteraciones en los Collection Set y List

--- colecciones/src/com/coderroan/ejemplos/set/EjemploHashSetUnicidad.java
+++ colecciones/src/com/coderroan/ejemplos/set/EjemploHashSetUnicidad.java
@@ -2,12 +2,12 @@ package com.coderroan.ejemplos.set;
 
 import com.coderroan.ejemplos.modelo.Alumno;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class EjemploHashSetUnicidad {
     public static void main(String[] args) {
-        Set<Alumno> sa = new HashSet<>();
+        Set<Alumno> sa = new TreeSet<>();
+        //List<Alumno> sa = new LinkedList<>();
         sa.add(new Alumno("Eliceo", 9));
         sa.add(new Alumno("Eliceo2", 9));
         sa.add(new Alumno("Raul", 7));
@@ -18,5 +18,23 @@ public class EjemploHashSetUnicidad {
         sa.add(new Alumno("Luis", 5));
         sa.add(new Alumno("Luis", 6));
         System.out.println("Alumnos = " + sa);
+        /*System.out.println(Color.GREEN + "Iterando usando un for clasico" + Color.RESET);
+        for (int i = 0; i<sa.size(); i++){
+            Alumno a = sa.get(i);
+            System.out.println(a.getNombre());
+        }*/
+        System.out.println(Color.CYAN + "Iterando usando un foreach" + Color.RESET);
+        for (Alumno a : sa ) {
+            System.out.println(a.getNombre());
+        }
+        System.out.println(Color.BLUE + "Iterando usando un while e Iterator" + Color.RESET);
+        Iterator<Alumno> it = sa.iterator();
+        while (it.hasNext()){
+            Alumno a = it.next();
+            System.out.println(a.getNombre());
+        }
+        System.out.println(Color.BLACK_BACKGROUND_BRIGHT + "Utilizando expresion lambda Stream forEach" + Color.RESET);
+        sa.forEach(System.out::println);
+
     }
 }
