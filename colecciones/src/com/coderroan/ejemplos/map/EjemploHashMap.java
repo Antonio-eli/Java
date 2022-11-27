30de520dbc86f62a7b546c97f8257086ff4e2df7
Antonio-eli <eliceoroan@gmail.com>
2022-11-27 16:38:53 -0600

    Add HashMap tipo Object y relaciones de objetos

--- colecciones/src/com/coderroan/ejemplos/map/EjemploHashMap.java
+++ colecciones/src/com/coderroan/ejemplos/map/EjemploHashMap.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 public class EjemploHashMap {
     public static void main(String[] args) {
-        Map<String, String> persona = new HashMap<>();
+        Map<String, Object> persona = new HashMap<>();
         System.out.println(Color.WHITE_UNDERLINED + "Contiene elementos = " + !persona.isEmpty() + Color.RESET);
         persona.put(null, "1234");
         persona.put("Nombre", "Eliceo");
@@ -17,19 +17,38 @@ public class EjemploHashMap {
         persona.put("ApellidoPaterno", "Romero");
         persona.put("Email", "eliceoroan@gmail.com");
         persona.put("Edad", "25");
+
+        Map<String, String> direccion = new HashMap<>();
+        direccion.put("pais","Mexico");
+        direccion.put("estado","CDMx");
+        direccion.put("ciudad","Alvaro Obregon");
+        direccion.put("calle","1CDA San Diego");
+        direccion.put("numero","12");
+
+        persona.put("direccion", direccion);
         System.out.println("Persona: " +  persona);
 
         /*Obtener valores espefificos del HashMap*/
-        String nombre = persona.get("Nombre");
+        String nombre = (String) persona.get("Nombre");
         System.out.println("nombre = " + nombre);
-        String apellido = persona.get("Apellido");
+        String apellido = (String) persona.get("Apellido");
         System.out.println("apellido = " + apellido);
+
+        /* Ejemplo HashMap con valor del tipo Object y relaciones de objetos */
+        System.out.println(Color.BLUE_BOLD + ":: HashMap tipo Object y relaciones de objetos ::" + Color.RESET);
+        Map<String, String> direccionPersona = (Map<String, String>)persona.get("direccion");
+        String pais = direccionPersona.get("pais");
+        String ciudad = direccionPersona.get("ciudad");
+        String barrio = direccionPersona.getOrDefault("barrio","Barrio norte");
+        System.out.println("El pais de " + nombre + " es: " + pais);
+        System.out.println("La ciudad de " + nombre + " es: " + ciudad);
+        System.out.println("El barrio de " + nombre + " es: " + barrio);
 
         /*HashMap métodos más importates*/
 
         System.out.println(Color.BLUE_BOLD + ":: Metodo remove ::" + Color.RESET);
-        String email = persona.remove("Email");
-        //boolean email = persona.remove("Email","eliceoroan@gmail.com");
+        //String email = persona.remove("Email");
+        boolean email = persona.remove("Email","eliceoroan@gmail.com");
         System.out.println("Eliminado: " +  email);
         System.out.println("Persona: " +  persona);
 
@@ -42,8 +61,8 @@ public class EjemploHashMap {
         System.out.println(Color.RED + "Existe el valor de Romero en el HashMap= " + b + Color.RESET);
 
         System.out.println(Color.BLUE_BOLD + ":: Metodo values ::" + Color.RESET);
-        Collection<String> valores = persona.values();
-        for (String v : valores ) {
+        Collection<Object> valores = persona.values();
+        for (Object v : valores ) {
             System.out.println("v = " + v);
         }
 
@@ -54,14 +73,14 @@ public class EjemploHashMap {
         }
 
         System.out.println(Color.BLUE_BOLD + ":: Metodo entrySet ::" + Color.RESET);
-        for (Map.Entry<String, String> par : persona.entrySet()){
+        for (Map.Entry<String, Object> par : persona.entrySet()){
             System.out.println(par.getKey() + " => " + par.getValue());
         }
 
         System.out.println(Color.BLUE_BOLD + ":: Alternativa simple ::" + Color.RESET);
 
         for (String llave : persona.keySet()) {
-            String valor = persona.get(llave);
+            Object valor = persona.get(llave);
             System.out.println(Color.YELLOW + llave + " -> " + valor + Color.RESET);
         }
 
