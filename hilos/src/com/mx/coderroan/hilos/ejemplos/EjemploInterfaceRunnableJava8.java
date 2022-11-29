b0c5dfbd1cb4177fd08c246294916a8603d3d7f9
Antonio-eli <eliceoroan@gmail.com>
2022-11-29 00:31:42 -0600

    Add metodo join vs sleep

--- hilos/src/com/mx/coderroan/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
+++ hilos/src/com/mx/coderroan/hilos/ejemplos/EjemploInterfaceRunnableJava8.java
@@ -3,7 +3,8 @@ package com.mx.coderroan.hilos.ejemplos;
 import com.mx.coderroan.hilos.ejemplos.runnable.ViajeTarea;
 
 public class EjemploInterfaceRunnableJava8 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Thread main = Thread.currentThread();
         Runnable viaje = () -> {
                 for (int i = 0; i <= 10; i++){
                     System.out.println(i + " - " + Thread.currentThread().getName());
@@ -14,12 +15,28 @@ public class EjemploInterfaceRunnableJava8 {
                     }
                 }
                 System.out.println(Color.CYAN + "Finalmente me voy de viaje a: " + Thread.currentThread().getName() + Color.RESET);
+                System.out.println(main.getState());
         };
 
-        new Thread(viaje, "Isla Cabo Santa Teresa").start();
-        new Thread(viaje,"Isla del Carmen").start();
-        new Thread(viaje,"Isla Quintín Aráuz").start();
-        new Thread(viaje,"Isla Cozumel").start();
-        new Thread(viaje,"Isla Cancún").start();
+        Thread v1 = new Thread(viaje, "Isla Cabo Santa Teresa");
+        Thread v2 = new Thread(viaje,"Isla del Carmen");
+        Thread v3 = new Thread(viaje,"Isla Quintín Aráuz");
+        Thread v4 = new Thread(viaje,"Isla Cozumel");
+        Thread v5 = new Thread(viaje,"Isla Cancún");
+
+        v1.start();
+        v2.start();
+        v3.start();
+        v4.start();
+        v5.start();
+        v1.join();
+        v2.join();
+        v3.join();
+        v4.join();
+        v5.join();
+
+        //Thread.sleep(5000);
+        System.out.println(Color.BLUE_BOLD + "Continuando con la ejecución del método main: " + main.getName() + Color.RESET);
+
     }
 }
