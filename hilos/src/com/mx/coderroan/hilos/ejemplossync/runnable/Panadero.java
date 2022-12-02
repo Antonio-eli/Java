ec2e3679db56d6c8507e5843563e0b738efdcc24
Antonio-eli <eliceoroan@gmail.com>
2022-12-02 12:51:26 -0600

    Add Consumidor con Java 8 Lambda

--- hilos/src/com/mx/coderroan/hilos/ejemplossync/runnable/Panadero.java
+++ hilos/src/com/mx/coderroan/hilos/ejemplossync/runnable/Panadero.java
@@ -13,13 +13,6 @@ public class Panadero implements Runnable {
 
     @Override
     public void run() {
-        for(int i = 0; i < 10; i++){
-            panaderia.hornear("Pan n°: " + i);
-            try {
-                Thread.sleep(ThreadLocalRandom.current().nextInt(500, 2000));
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+
     }
 }
