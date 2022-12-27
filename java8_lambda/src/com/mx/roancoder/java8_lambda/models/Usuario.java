f4201856c8b0e9979191b12acb6fbd83ea6c808e
Eliceo <eliceo.romero@totalplay.com.mx>
2022-12-27 15:30:28 -0600

    Add Referencias de metodos en expresiones lambda de tipo Consumer

---
+++ java8_lambda/src/com/mx/roancoder/java8_lambda/models/Usuario.java
@@ -0,0 +1,13 @@
+package com.mx.roancoder.java8_lambda.models;
+
+public class Usuario {
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String nombre;
+}
