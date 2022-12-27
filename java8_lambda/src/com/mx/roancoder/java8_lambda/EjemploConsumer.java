d397b15ca12c087ea138ad7c899625965b162d36
Eliceo <eliceo.romero@totalplay.com.mx>
2022-12-27 15:48:52 -0600

    Add Expresiones Lambda Function v1

--- java8_lambda/src/com/mx/roancoder/java8_lambda/EjemploConsumer.java
+++ java8_lambda/src/com/mx/roancoder/java8_lambda/EjemploConsumer.java
@@ -1,9 +1,15 @@
 package com.mx.roancoder.java8_lambda;
 
+import com.mx.roancoder.java8_lambda.models.Usuario;
+
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class EjemploConsumer {
     public static void main(String[] args) {
@@ -13,9 +19,22 @@ public class EjemploConsumer {
         };
 
         consumer.accept(new Date());
-        BiConsumer<String, Integer> consumerBi = (nombre, edad) -> {
-            System.out.println(nombre + ", tiene " + edad + " anos!");
-        };
+        BiConsumer<String, Integer> consumerBi = (nombre, edad) -> System.out.println(nombre + ", tiene " + edad + " anos!");
         consumerBi.accept("Eliceo", 25);
+
+        Consumer<String> consumer2 = System.out::println;
+        consumer2.accept("Hola mundo lambda");
+
+        List<String> nombres = Arrays.asList("Eliceo", "Elias", "Raul");
+        nombres.forEach(consumer2);
+
+        Supplier<Usuario> creaUsuario = Usuario::new;
+        Usuario usuario = creaUsuario.get();
+        BiConsumer<Usuario, String> asignarNombre = Usuario::setNombre;
+        asignarNombre.accept(usuario, "Luis");
+        System.out.println("Nombre usuario: " + usuario.getNombre());
+
+        Supplier<String> proveedor = () -> "Hola mundo lambda Supplier";
+        System.out.println(proveedor.get());
     }
 }
