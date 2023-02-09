203210142238db22af820c5656107ca0bae9ade8
ALAIN ARTURO MARTINEZ GARCIA <aamartinezg@totalplay.corp>
2023-02-09 01:09:17 -0600

    Add operador map parte 2

--- api_stream/src/com/mx/roancoder/api/stream/ejemplos/EjemploStreamMap.java
+++ api_stream/src/com/mx/roancoder/api/stream/ejemplos/EjemploStreamMap.java
@@ -1,18 +1,27 @@
 package com.mx.roancoder.api.stream.ejemplos;
 
+import com.mx.roancoder.api.stream.ejemplos.models.Usuario;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class EjemploStreamMap {
     public static void main(String[] args) {
-        Stream<String> nombres = Stream.of("Pato ", "Pepe","Luis","Angel")
+        Stream<Usuario> nombres = Stream
+                .of("Pato Guzman", "Pepe Gonzalez","Luis Villa","Angel Salsa")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
                 .peek(System.out::println)
-                .map(nombre -> {
-                    return nombre.toUpperCase();
-                })
-                .map(e -> e.toLowerCase());
-        nombres.forEach(System.out::println);
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toUpperCase();
+                    usuario.setNombre(nombre);
+                    return usuario;
+                });
+
+        List<Usuario> lista = nombres.collect(Collectors.toList());
+        lista.forEach(System.out::println);
+        //nombres.forEach(System.out::println);
     }
 }
